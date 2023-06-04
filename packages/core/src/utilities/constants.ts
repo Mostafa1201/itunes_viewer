e8478fe810cceacd7e60a3e4fc9d02edf82b3081
Mostafa1201 <mostafa1201@hotmail.com>
2023-06-04 19:08:09 +0300

    # feat - Add all constants in a file

---
+++ packages/core/src/utilities/constants.ts
@@ -0,0 +1,6 @@
+export abstract class Constants {
+    static readonly TracksTableName = 'Tracks';
+    static readonly AppName = 'itunes-search-backend';
+    static readonly Region = 'eu-central-1';
+    static readonly ItunesURI = 'https://itunes.apple.com';
+}
