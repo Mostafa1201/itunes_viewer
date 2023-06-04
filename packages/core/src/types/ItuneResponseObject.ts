064f4444ee32d6845fb30e4039bae93631fa3894
Mostafa1201 <mostafa1201@hotmail.com>
2023-06-04 19:09:55 +0300

    # feat - Add dynamodb storage stack for track table

--- packages/core/src/types/ItuneResponseObject.ts
+++ packages/core/src/types/ItuneResponseObject.ts
@@ -2,19 +2,20 @@ type ItuneResponseObject = {
     kind: string; // The kind of content returned by the search request.
     artistName: string;
     collectionName: string;
+    trackId: number;
     trackName: string; // The name of the track, song, video, TV episode, and so on returned by the search request.
     trackViewUrl: string;
+    trackCount: number; // The number of tracks in the album.
+    trackPrice: number;
+    trackTimeMillis: number;
     previewUrl: string;
     collectionPrice: number;
-    trackPrice: number;
     releaseDate: string;
     country: string;
     currency: string;
     primaryGenreName: string;
-    contentAdvisoryRating: string;
     shortDescription: string;
     longDescription: string;
-    [key: string]: any;
 }
 
 export default ItuneResponseObject;
